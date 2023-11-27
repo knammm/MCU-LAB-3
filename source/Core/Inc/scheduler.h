31b051a19f58eccf4f8bc6ad600b5f7e29fc0aef
Nam Nguyen <nam.nguyenolkmphy@hcmut.edu.vn>
2023-11-27 23:49:53 +0700

    Optimize scheduler

--- source/Core/Inc/scheduler.h
+++ source/Core/Inc/scheduler.h
@@ -14,17 +14,10 @@
 #define SCH_MAX_TASKS 	10
 #define	NO_TASK_ID		0
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+void SCH_Init(void);
+void SCH_Update(void);
+void SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
+void SCH_Dispatch_Tasks(void);
+void SCH_Delete_Task(uint32_t TASK_ID);
 
-    void SCH_Init(void);
-    void SCH_Update(void);
-    uint32_t SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
-    void SCH_Dispatch_Tasks(void);
-    uint8_t SCH_Delete_Task(uint32_t TASK_ID);
-
-#ifdef __cplusplus
-}
-#endif
 #endif /* INC_SCHEDULER_H_ */
--- source/Core/Src/scheduler.c
+++ source/Core/Src/scheduler.c
@@ -8,10 +8,6 @@
 
 #include "scheduler.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef struct {
 	void ( * pTask)(void);
 	uint32_t Delay;
@@ -21,127 +17,121 @@ typedef struct {
 } sTask;
 
 // The array of tasks
-static sTask SCH_tasks_G[SCH_MAX_TASKS];
-//static uint8_t array_Of_Task_ID[SCH_MAX_TASKS];
+static sTask SCH_tasks_Array[SCH_MAX_TASKS];
 static uint32_t newTaskID = 0;
-//static uint32_t rearQueue = 0;
-static uint32_t count_SCH_Update = 0;
 
 
 static uint32_t Get_New_Task_ID(void);
-//static void TIMER_Init();
 
 
 void SCH_Init(void){
     for (int i=0; i<SCH_MAX_TASKS; i++){
-        SCH_tasks_G[i].pTask = 0;
-        SCH_tasks_G[i].Delay = 0;
-        SCH_tasks_G[i].Period = 0;
-        SCH_tasks_G[i].RunMe = 0;
+        SCH_tasks_Array[i].pTask = 0;
+        SCH_tasks_Array[i].Delay = 0;
+        SCH_tasks_Array[i].Period = 0;
+        SCH_tasks_Array[i].RunMe = 0;
 
-        SCH_tasks_G[i].TaskID = -1;
+        SCH_tasks_Array[i].TaskID = -1;
     }
 }
 
 void SCH_Update(void){
-	// Check if there is a task at this location
-	count_SCH_Update ++;
-	if (SCH_tasks_G[0].pTask && SCH_tasks_G[0].RunMe == 0) {
-		if(SCH_tasks_G[0].Delay > 0){
-			SCH_tasks_G[0].Delay = SCH_tasks_G[0].Delay - 1;
+	if (SCH_tasks_Array[0].pTask && SCH_tasks_Array[0].RunMe == 0) {
+		if(SCH_tasks_Array[0].Delay > 0){
+			SCH_tasks_Array[0].Delay = SCH_tasks_Array[0].Delay - 1;
 		}
-		if (SCH_tasks_G[0].Delay == 0) {
-			SCH_tasks_G[0].RunMe = 1;
+		if (SCH_tasks_Array[0].Delay == 0) {
+			SCH_tasks_Array[0].RunMe = 1;
 		}
 	}
 }
-uint32_t SCH_Add_Task(void (* pFunction)(), uint32_t DELAY, uint32_t PERIOD){
-	uint8_t newTaskIndex = 0;
-	uint32_t sumDelay = 0;
-	uint32_t newDelay = 0;
 
-	for(newTaskIndex = 0; newTaskIndex < SCH_MAX_TASKS; newTaskIndex ++){
-		sumDelay = sumDelay + SCH_tasks_G[newTaskIndex].Delay;
-		if(sumDelay > DELAY){
-			newDelay = DELAY - (sumDelay - SCH_tasks_G[newTaskIndex].Delay);
-			SCH_tasks_G[newTaskIndex].Delay = sumDelay - DELAY;
-			for(uint8_t i = SCH_MAX_TASKS - 1; i > newTaskIndex; i --){
-//				if(SCH_tasks_G[i - 1].pTask != 0)
-				{
-					SCH_tasks_G[i].pTask = SCH_tasks_G[i - 1].pTask;
-					SCH_tasks_G[i].Period = SCH_tasks_G[i - 1].Period;
-					SCH_tasks_G[i].Delay = SCH_tasks_G[i - 1].Delay;
-//					SCH_tasks_G[i].RunMe = SCH_tasks_G[i - 1].RunMe;
-					SCH_tasks_G[i].TaskID = SCH_tasks_G[i - 1].TaskID;
-				}
-			}
-			SCH_tasks_G[newTaskIndex].pTask = pFunction;
-			SCH_tasks_G[newTaskIndex].Delay = newDelay;
-			SCH_tasks_G[newTaskIndex].Period = PERIOD;
-			if(SCH_tasks_G[newTaskIndex].Delay == 0){
-				SCH_tasks_G[newTaskIndex].RunMe = 1;
-			} else {
-				SCH_tasks_G[newTaskIndex].RunMe = 0;
-			}
-			SCH_tasks_G[newTaskIndex].TaskID = Get_New_Task_ID();
-			return SCH_tasks_G[newTaskIndex].TaskID;
-		} else {
-			if(SCH_tasks_G[newTaskIndex].pTask == 0x0000){
-				SCH_tasks_G[newTaskIndex].pTask = pFunction;
-				SCH_tasks_G[newTaskIndex].Delay = DELAY - sumDelay;
-				SCH_tasks_G[newTaskIndex].Period = PERIOD;
-				if(SCH_tasks_G[newTaskIndex].Delay == 0){
-					SCH_tasks_G[newTaskIndex].RunMe = 1;
-				} else {
-					SCH_tasks_G[newTaskIndex].RunMe = 0;
-				}
-				SCH_tasks_G[newTaskIndex].TaskID = Get_New_Task_ID();
-				return SCH_tasks_G[newTaskIndex].TaskID;
-			}
-		}
-	}
-	return SCH_tasks_G[newTaskIndex].TaskID;
+void SCH_Add_Task(void (*pFunction)(), uint32_t DELAY, uint32_t PERIOD) {
+    uint8_t newTaskIndex = 0;
+    uint32_t sumDelay = 0;
+    uint32_t newDelay = 0;
+
+    // Iterate through the task array to find the appropriate position for the new task
+    for (newTaskIndex = 0; newTaskIndex < SCH_MAX_TASKS; newTaskIndex++) {
+        // Calculate the cumulative delay of existing tasks
+        sumDelay += SCH_tasks_Array[newTaskIndex].Delay;
+
+        // Check if the cumulative delay is greater than the specified initial delay (DELAY)
+        if (sumDelay > DELAY) {
+            // Calculate the new delay for the current task
+            newDelay = DELAY - (sumDelay - SCH_tasks_Array[newTaskIndex].Delay);
+
+            // Adjust the delay of the current task and shift existing tasks to make room
+            SCH_tasks_Array[newTaskIndex].Delay = sumDelay - DELAY;
+            for (uint8_t i = SCH_MAX_TASKS - 1; i > newTaskIndex; i--) {
+                SCH_tasks_Array[i] = SCH_tasks_Array[i - 1];
+            }
+
+            // Add the new task at the appropriate position in the array
+            SCH_tasks_Array[newTaskIndex] = (sTask){
+                .pTask = pFunction,
+                .Delay = newDelay,
+                .Period = PERIOD,
+                .RunMe = (newDelay == 0),  // Set RunMe flag based on newDelay
+                .TaskID = Get_New_Task_ID(),  // Assign a unique TaskID
+            };
+
+            return;
+        } else {
+            // If the current task slot is empty, add the new task to this slot
+            if (SCH_tasks_Array[newTaskIndex].pTask == 0) {
+                SCH_tasks_Array[newTaskIndex] = (sTask){
+                    .pTask = pFunction,
+                    .Delay = DELAY - sumDelay,
+                    .Period = PERIOD,
+                    .RunMe = (DELAY - sumDelay == 0),  // Set RunMe flag based on remaining delay
+                    .TaskID = Get_New_Task_ID(),  // Assign a unique TaskID
+                };
+
+                return;
+            }
+        }
+    }
 }
 
 
-uint8_t SCH_Delete_Task(uint32_t taskID){
-	uint8_t Return_code  = 0;
-	uint8_t taskIndex;
-	uint8_t j;
-	if(taskID != NO_TASK_ID){
-		for(taskIndex = 0; taskIndex < SCH_MAX_TASKS; taskIndex ++){
-			if(SCH_tasks_G[taskIndex].TaskID == taskID){
-				Return_code = 1;
-				if(taskIndex != 0 && taskIndex < SCH_MAX_TASKS - 1){
-					if(SCH_tasks_G[taskIndex+1].pTask != 0x0000){
-						SCH_tasks_G[taskIndex+1].Delay += SCH_tasks_G[taskIndex].Delay;
-					}
-				}
 
-				for( j = taskIndex; j < SCH_MAX_TASKS - 1; j ++){
-					SCH_tasks_G[j].pTask = SCH_tasks_G[j+1].pTask;
-					SCH_tasks_G[j].Period = SCH_tasks_G[j+1].Period;
-					SCH_tasks_G[j].Delay = SCH_tasks_G[j+1].Delay;
-					SCH_tasks_G[j].RunMe = SCH_tasks_G[j+1].RunMe;
-					SCH_tasks_G[j].TaskID = SCH_tasks_G[j+1].TaskID;
-				}
-				SCH_tasks_G[j].pTask = 0;
-				SCH_tasks_G[j].Period = 0;
-				SCH_tasks_G[j].Delay = 0;
-				SCH_tasks_G[j].RunMe = 0;
-				SCH_tasks_G[j].TaskID = 0;
-				return Return_code;
-			}
-		}
-	}
-	return Return_code; // return status
+void SCH_Delete_Task(uint32_t taskID) {
+    // Check if the provided task ID is NO_TASK_ID (invalid).
+    if (taskID == NO_TASK_ID) return;
+
+    // Iterate through the task array to find the task with the specified TaskID
+    for (uint8_t taskIndex = 0; taskIndex < SCH_MAX_TASKS; taskIndex++) {
+        if (SCH_tasks_Array[taskIndex].TaskID == taskID) {
+            // Task found, proceed with deletion
+
+            // If the current task is not the last one and the next task is not empty,
+            // adjust the delay of the next task to include the delay of the deleted task.
+            if (taskIndex < SCH_MAX_TASKS - 1 && SCH_tasks_Array[taskIndex + 1].pTask != 0) {
+                SCH_tasks_Array[taskIndex + 1].Delay += SCH_tasks_Array[taskIndex].Delay;
+            }
+
+            // Shift the remaining tasks to fill the gap created by the deleted task
+            for (uint8_t j = taskIndex; j < SCH_MAX_TASKS - 1; j++) {
+                SCH_tasks_Array[j] = SCH_tasks_Array[j + 1];
+            }
+
+            // Clear the last task slot to ensure no leftover data
+            SCH_tasks_Array[SCH_MAX_TASKS - 1] = (sTask){0};
+
+            return;
+        }
+    }
+
+    return; // Task ID not found, exit the function
 }
+
 
 void SCH_Dispatch_Tasks(void){
-	if(SCH_tasks_G[0].RunMe > 0) {
-		(*SCH_tasks_G[0].pTask)(); // Run the task
-		SCH_tasks_G[0].RunMe = 0; // Reset RunMe flag
-		sTask temtask = SCH_tasks_G[0];
+	if(SCH_tasks_Array[0].RunMe > 0) {
+		(*SCH_tasks_Array[0].pTask)(); // Function pointer -> Run task
+		SCH_tasks_Array[0].RunMe = 0; // Set back RunMe flag
+		sTask temtask = SCH_tasks_Array[0];
 		SCH_Delete_Task(temtask.TaskID);
 		if (temtask.Period != 0) {
 			SCH_Add_Task(temtask.pTask, temtask.Period, temtask.Period);
@@ -149,14 +139,8 @@ void SCH_Dispatch_Tasks(void){
 	}
 }
 
-static uint32_t Get_New_Task_ID(void){
-	newTaskID++;
-	if(newTaskID == NO_TASK_ID){
-		newTaskID++;
-	}
-	return newTaskID;
+static uint32_t Get_New_Task_ID(void) {
+    return ++newTaskID;
 }
 
-#ifdef __cplusplus
-}
-#endif
+
